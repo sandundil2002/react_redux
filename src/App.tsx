1bda684db85feb0d1a12a565b90f0769e8ce7715
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2025-01-03 10:42:09 +0530

    toggle button function implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import {useDispatch, useSelector} from "react-redux";
 
 function App() {
 
-    const count = useSelector(state => state);
+    const {count, toggle} = useSelector(state => state);
 
     const dispatch = useDispatch();
 
   return (
     <>
-        {count}
+        {toggle && <h1>{count}</h1>}
         <br/>
         <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
         <button onClick={() => dispatch({type: 'DECREMENT'})}>Decrement</button>
--- src/reducers/CountReducer.tsx
+++ src/reducers/CountReducer.tsx
@@ -1,14 +1,17 @@
-export const initialState = 0;
+export const initialState = {
+    count: 0,
+    toggle: true
+}
 
-export const CountReducer = (
-    state = initialState, action) => {
+export const CountReducer = (state= initialState , action:{type:string}) => {
     switch (action.type) {
         case 'INCREMENT':
-            return state + 1;
+            return {...state, count: state.count + 1}
         case 'DECREMENT':
-            return state - 1;
+            return {...state, count: state.count - 1}
         case 'TOGGLE':
+            return {...state, toggle: !state.toggle}
         default:
-            return state;
+            return state
     }
 }
